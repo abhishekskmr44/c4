0f59a54a793bc53f99e46230e056cea24956e8cb
abhishek sukumar <abhishekskm44@gmail.com>
2022-05-02 17:14:24 +0530

    init

---
+++ src/components/Styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Grid = styled.div`
+width:85%;
+margin:auto;
+display:grid;
+padding:10px;
+grid-template-columns:repeat(4,1fr);
+grid-template-row :auto`;
+
+export const Select = styled.select``;
+
+export const Flex = styled.div``;
